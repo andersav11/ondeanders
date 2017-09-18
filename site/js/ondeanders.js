98054e65d709e55ac3fe65362b36cf6060b3f295
andersav11 <anders.av11@gmail.com>
2017-09-18 11:56:52 -0300

    espero que role de colocar o js

---
+++ site/js/ondeanders.js
@@ -0,0 +1,8 @@
+function myfunction1() {document.getElementById("telae").innerHTML = "Olá, sou eu<br><br>
+			Criei este site para mostrar um pouco
+			do que faço em meus trabalhos. <br><br>
+			Explore o que estiver na mesa para descobrir
+			o que tenho feito!";
+}
+
+mufunction1;
